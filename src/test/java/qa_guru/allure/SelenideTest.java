fa74bb109af860e541a8eaa8a91295135ae0c502
bayron013 <bayron013@gmail.com>
2026-05-04 16:53:47 +0300

    Создал новый тест класс и внёс конфигурационные правки

--- src/test/java/qa_guru/allure/SelenideTest.java
+++ src/test/java/qa_guru/allure/SelenideTest.java
@@ -1,4 +1,35 @@
 package qa_guru.allure;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
 public class SelenideTest {
+
+    @BeforeAll
+    public static void SetUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    public void testIssueSearch() {
+        open("https://github.com");
+
+        $(".search-input").click();
+        $("#query-builder-test").setValue("eroshenkoam/allure");
+        $("#query-builder-test").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#_R_dsraqdb_--label").click();
+
+        System.out.println("nou");
+    }
+
+
 }
