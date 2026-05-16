6cfe2acd0794c2749cb30a6fb8c3ef0eb0009aa0
Simanin Vladislav <bayron013@gmail.com>
2026-05-16 13:31:19 +0300

    create a new class "lambda"

--- src/test/java/qa_guru/allure/SelenideTest.java
+++ src/test/java/qa_guru/allure/SelenideTest.java
@@ -2,6 +2,8 @@ package qa_guru.allure;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +22,8 @@ public class SelenideTest {
 
     @Test
     public void testIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         open("https://github.com");
 
         $(".search-input").click();
@@ -27,7 +31,7 @@ public class SelenideTest {
         $("#query-builder-test").submit();
         $(linkText("eroshenkoam/allure-qaguru")).click();
         $("#issues-tab").click();
-        $(withText("Заменяем степы на Listener")).should(Condition.exist);
+        $(withText("Заменяем степы на Listenerrr")).should(Condition.exist);
 
     }
 
--- src/test/java/qa_guru/allure/StepsTest.java
+++ src/test/java/qa_guru/allure/StepsTest.java
@@ -1,4 +1,30 @@
 package qa_guru.allure;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
 public class StepsTest {
+
+    @Test
+    public void testLambda() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".search-input").click();
+        $("#query-builder-test").setValue("eroshenkoam/allure");
+        $("#query-builder-test").submit();
+        $(linkText("eroshenkoam/allure-qaguru")).click();
+        $("#issues-tab").click();
+        $(withText("Заменяем степы на Listenerrr")).should(Condition.exist);
+
+    }
+
 }
